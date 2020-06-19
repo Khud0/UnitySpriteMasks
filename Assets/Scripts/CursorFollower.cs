32ced9dcfa86a779775b66acaed5aa383fbe6fe5
Khud0 <pavement.cafe.37@gmail.com>
2020-06-19 14:47:26 +0200

    Initial Commit

---
+++ Assets/Scripts/CursorFollower.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class CursorFollower : MonoBehaviour
+{
+    private Camera cam;
+
+    private void Start()
+    {
+        // Calling Camera.main in the Update function is inefficeint.
+        // It's usually better to cache a reference to it.
+        cam = Camera.main;
+    }
+    private void Update()
+    {
+        // Change cursor's position every frame.
+        FollowCursor();   
+    }
+
+    private void FollowCursor()
+    {
+        // Convert position of your cursor to a point on the game's screen.
+        Vector2 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        // Apply calculated position to the object this script is attached to.
+        this.transform.position = mousePos;
+    }
+}
